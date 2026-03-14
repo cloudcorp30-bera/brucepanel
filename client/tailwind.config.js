61d4cac21d863e857d54ae578b24ab809252c184
cloudcorp30-bera <cloudcorp30@gmail.com>
2026-03-14 12:04:28 +0300

    add client/tailwind.config.js

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: { 900: "#0a0a0f", 800: "#111118", 700: "#1a1a24", 600: "#22222f", 500: "#2d2d3e" }
+      }
+    }
+  },
+  plugins: []
+};
